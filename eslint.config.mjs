b8b278222581194a28edf1a86e205d0acfce66dd
Efraín Hernández <haefrain@gmail.com>
2026-06-10 20:17:06 -0500

    chore: bootstrap pnpm monorepo tooling (eslint, prettier, typescript)

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import vue from 'eslint-plugin-vue';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['**/dist/**', '**/coverage/**', '**/node_modules/**'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...vue.configs['flat/recommended'],
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+      },
+    },
+  },
+  {
+    files: ['**/*.{js,cjs}'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: globals.node,
+    },
+  },
+  prettier,
+);
